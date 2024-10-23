e5761d2bace32d6bd5dd9f50c7f267d9f727b9c4
XXMA <xyrus.malcala@gmail.com>
2024-10-23 10:49:24 -0600

    Updated handling of diverse datasets

--- bin/phyg_add_taxa.py
+++ bin/phyg_add_taxa.py
@@ -1,20 +1,136 @@
 #!/usr/bin/env python3
 
-import subprocess
+import logging, shutil, subprocess, sys
+
+from collections import defaultdict
 
 from pathlib import Path
 
+import largest_isoform as liso
+import phyg_orf_related as oc
+
 from Bio import SeqIO
+
+"""
+notes to self:
+
+if given ORFs from WGS file (from GenBank/RefSeq), must extract the accession codes and can
+ignore the minimum length requirements
+
+Similarly, if given a file of ORFs, no need to manipulate the sequences inside...
+-- just add the taxon_name and delimiter IF the seq-id doesn't start with taxon_name
+"""
+
+def back_up_file(
+        out_dir: str,
+        fasta_file: str) -> None:
+
+    Path(out_dir).mkdir(exist_ok = True, parents = True)
+
+    shutil.copy2(fasta_file, out_dir)
+
+
+def eval_wgs_genbank(
+        out_dir: str,
+        taxon_name: str,
+        fasta_file: str,
+        taxon_code: str = None,
+        prokaryotic: bool = False,
+        remove_isoforms: bool = True,
+        delim: str = '|') -> str:
+
+    Path(out_dir).mkdir(exist_ok = True, parents = True)
+
+
+    final_filt_fas = f'{out_dir}{taxon_name}.LongestIsoform'
+
+    if taxon_code:
+        final_filt_fas = final_filt_fas.replace(taxon_name, taxon_code)
+
+    if not remove_isoforms:
+        final_filt_fas.replace(".LongestIsoform",".ORFs")
+
+    final_seqs = []
+    name_conversion = {}
+
+    if remove_isoforms:
+        isoforms_to_keep = liso.save_isoforms(fasta_file, True, prokaryotic)
+    else:
+        isoforms_to_keep = liso.save_isoforms(fasta_file, False, prokaryotic)
+
+    for i in isoforms_to_keep:
+        seq_name = f'{taxon_name}{delim}{i.id}'
+        if taxon_code:
+            seq_name = f'{taxon_code}{delim}{seq_name}'
+        name_conversion[i.id] = seq_name
+        i.id = seq_name
+        i.description = ''
+        i.name = ''
+        final_seqs.append(i)
+
+    SeqIO.write(final_seqs, f'{final_filt_fas}.NTD.fasta', 'fasta')
+
+    with open(f'{final_filt_fas}.NameConversion.tsv', 'w+') as w:
+        w.write('Initial_ID\tUpdated_Name\n')
+        for k, v in name_conversion.items():
+            w.write(f'{k}\t{v}\n')
+
+    return f'{final_filt_fas}.NTD.fasta'
+
+
+def eval_wgs_non_gbk(
+        out_dir: str,
+        taxon_name: str,
+        fasta_file: str,
+        taxon_code: str,
+        prokaryotic: bool = False,
+        delim: str = '|'
+        ) -> str:
+
+
+    Path(out_dir).mkdir(exist_ok = True, parents = True)
+
+    final_filt_fas = f'{out_dir}{taxon_name}.ORFs.fasta'
+
+    if taxon_code:
+        final_filt_fas = final_filt_fas.replace(taxon_name, taxon_code)
+
+    final_seqs = []
+    name_conversion = {}
+
+    for i in SeqIO.parse(fasta_file,'fasta'):
+        if len(i.seq)%3 == 0:
+            seq_name = f'{taxon_name}{delim}{i.id.split(delim)[-1]}'
+
+            if taxon_code:
+                seq_name = f'{taxon_code}{delim}{seq_name}'
+
+            name_conversion[i.id] = seq_name
+            i.id = seq_name
+            i.description = ''
+            i.name = ''
+            final_seqs.append(i)
+
+        else:
+            pass
+
+    SeqIO.write(final_seqs, final_filt_fas, 'fasta')
+
+    with open(final_filt_fas.replace(".fasta",".NameConversion.tsv"), 'w+') as w:
+        w.write('Initial_ID\tUpdated_Name\n')
+        for k, v in name_conversion.items():
+            w.write(f'{k}\t{v}\n')
+
+    return f'{final_filt_fas}.NTD.fasta'
 
 
 def remove_short_seqs(
         out_dir: str,
         taxon_name: str,
+        taxon_code: str,
         fasta_file: str,
         min_length: int = 200,
-        delim: str = '|',
-        transcript: bool = True,
-        wgs: bool = False) -> str:
+        delim: str = '|') -> str:
     """
     Remove short sequences from FASTA file
 
@@ -31,25 +147,32 @@ def remove_short_seqs(
     ----------
     size_filt_fas:  FASTA formatted file with short sequences removed
     """
+
+    Path(out_dir).mkdir(exist_ok = True, parents = True)
+
     seq_num = 1
 
     size_filt_seqs = []
     name_conversion = {}
 
     size_filt_fas = f'{out_dir}{taxon_name}.{min_length}bp.fasta'
+    if taxon_code:
+        size_filt_fas = size_filt_fas.replace(f'{out_dir}{taxon_name}',f'{out_dir}{taxon_code}')
 
     for i in SeqIO.parse(fasta_file,'fasta'):
         if len(i) >= min_length:
             seq_name = f'{taxon_name}{delim}'
-            if wgs:
-                # Append accession information for the ORF -- to do
-                pass
-            elif transcript:
-                seq_name += f'Transcript_{seq_num}_Length_{len(i)}'
-                seq_num += 1
-                if '_cov_' in i.id:
-                    kcov = f'{float(i.id.partition("_cov_")[-1].split("_")[0]):.2f}'
-                    seq_name += f'_Cov_{kcov}'
+
+            if taxon_code:
+                seq_name = f'{taxon_code}{delim}{seq_name}'
+
+            seq_name += f'Transcript_{seq_num}_Length_{len(i)}'
+            seq_num += 1
+
+            if '_cov_' in i.id:
+                kcov = f'{float(i.id.partition("_cov_")[-1].split("_")[0]):.2f}'
+                seq_name += f'_Cov_{kcov}'
+
             name_conversion[i.id] = seq_name
             i.id = seq_name
             i.description = ''
@@ -58,10 +181,17 @@ def remove_short_seqs(
 
     SeqIO.write(size_filt_seqs, size_filt_fas, 'fasta')
 
+    with open(size_filt_fas.replace(".fasta",".NameConversion.tsv"), 'w+') as w:
+        w.write('Initial_ID\tUpdated_Name\n')
+        for k, v in name_conversion.items():
+            w.write(f'{k}\t{v}\n')
+
     return size_filt_fas
 
 
-def run_barrnap(fasta_file: str, threads: int) -> list:
+def run_barrnap(
+        fasta_file: str,
+        threads: int) -> list:
     """
     Run Barrnap to remove easily identifiable rRNAs.
 
@@ -93,7 +223,12 @@ def run_barrnap(fasta_file: str, threads: int) -> list:
     return rRNA_seqs
 
 
-def remove_rRNA(out_dir: str, taxon_name: str, fasta_file: str,  min_len: int, threads: int) -> str:
+def remove_rRNA(
+        out_dir: str,
+        taxon_name: str,
+        fasta_file: str,
+        min_len: int,
+        threads: int) -> str:
     """
     Remove putative rRNA sequences from FASTA file
 
@@ -110,10 +245,12 @@ def remove_rRNA(out_dir: str, taxon_name: str, fasta_file: str,  min_len: int, t
     rRNA_clean_fas:  FASTA formatted file without putative rRNA sequences
     """
 
+    Path(out_dir).mkdir(exist_ok = True, parents = True)
+
     rRNA_seqs = run_barrnap(fasta_file, threads)
 
-    rRNA_clean_fas = f'{out_dir}{taxon_code}.{min_len}bp.Filt_rRNA.fas'
-    rRNA_fas = f'{out_dir}{taxon_code}.{min_len}bp.rRNA_Seqs.fas'
+    rRNA_clean_fas = f'{out_dir}{taxon_name}.{min_len}bp.rRNA_Filtered.fasta'
+    rRNA_fas = f'{out_dir}{taxon_name}.{min_len}bp.rRNA_Seqs.fasta'
 
     rRNA_contam, clean_seqs = [],[]
 
@@ -128,3 +265,304 @@ def remove_rRNA(out_dir: str, taxon_name: str, fasta_file: str,  min_len: int, t
     SeqIO.write(clean_seqs, rRNA_clean_fas, 'fasta')
 
     return rRNA_clean_fas
+
+
+def collapse_isoforms(
+        out_dir: str,
+        fasta_file: str,
+        pid: float = 0.97,
+        threads: int = 4) -> str:
+
+    Path(out_dir).mkdir(exist_ok = True, parents = True)
+
+    cdhit_out_fasta = f'{out_dir}{fasta_file.rpartition("/")[-1].rpartition(".NTD")[0]}.Clustered.NTD.fasta'
+
+    cdhit_cmd  = 'cdhit-est -M 0 -G 0 -aL 0.0005 -aS 1.0 ' \
+                f'-T {threads} ' \
+                f'-c {pid} ' \
+                f'-i {fasta_file} ' \
+                f'-o {cdhit_out_fasta}'
+
+    cdhit_rslt = subprocess.run(cdhit_cmd.split(),
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            universal_newlines=True)
+    return cdhit_out_fasta
+
+
+def orf_calling(
+            out_dir: str,
+            taxon_name: str,
+            fasta_file: str,
+            diamond_db: str,
+            delim: str = '|',
+            og_delim: str = '|',
+            gen_code: str = '1',
+            threads: int = 4,
+            evalue: float = 1e-10,
+            min_id: int = 0,
+            max_hits: int = 1,
+            min_hit_cover: int = 60,
+            min_qry_cover: int = 0,
+            blast_based: bool = True,
+            top_hits_only: bool = False,
+            prots: bool = False) -> str:
+
+
+    Path(out_dir).mkdir(exist_ok = True, parents = True)
+
+    if blast_based:
+        orf_call_fasta, og_hit_tsv = oc.orf_call_diamond(
+                                            out_dir,
+                                            fasta_file,
+                                            taxon_name,
+                                            diamond_db,
+                                            delim,
+                                            og_delim,
+                                            gen_code,
+                                            threads,
+                                            evalue,
+                                            min_id,
+                                            max_hits,
+                                            min_hit_cover,
+                                            min_qry_cover,
+                                            top_hits_only,
+                                            prots)
+
+        return orf_call_fasta, og_hit_tsv
+    else:
+        pass
+
+
+def prep_transcriptomes(
+        taxon_dir: str,
+        taxon_name: str,
+        taxon_code: str = None,
+        orf_db: str = None,
+        delim: str = '|',
+        og_delim: str = 'OG6',
+        min_len: int = 200,
+        gen_code: str = '1',
+        evalue: float = 1e-10,
+        min_id: int = 0,
+        max_hits: int = 1,
+        min_hit_cover: int = 60,
+        min_qry_cover: int = 0,
+        top_hits_only: bool = False,
+        blast_based: bool = True,
+        threads: int = 4):
+
+    out_dir_bu = f'{taxon_dir}/Original/'
+    out_dir_ss = f'{taxon_dir}/Filter_Steps/Length_Filter/'
+    out_dir_rr = f'{taxon_dir}/Filter_Steps/rRNA_Filter/'
+    out_dir_oc = f'{taxon_dir}/OG_ORF_Calling/'
+    out_dir_of = f'{taxon_dir}/Filter_Steps/Redundant_Transcript_Filter/'
+
+    tax = taxon_name
+
+    if taxon_code:
+        tax = taxon_code
+
+    back_up_file(
+        out_dir_bu,
+        fasta_file
+        )
+
+    sfilt_fasta = remove_short_seqs(
+                    out_dir_ss,
+                    taxon_name,
+                    taxon_code,
+                    fasta_file
+                    )
+
+    rrfilt_fasta = remove_rRNA(
+                        out_dir_rr,
+                        tax,
+                        sfilt_fasta,
+                        min_len,
+                        threads
+                        )
+
+    init_og_fasta, og_hit_tsv = orf_calling(
+                                    out_dir_oc,
+                                    tax,
+                                    rrfilt_fasta,
+                                    orf_db,
+                                    delim,
+                                    og_delim,
+                                    gen_code,
+                                    threads,
+                                    evalue,
+                                    min_id,
+                                    max_hits,
+                                    min_hit_cover,
+                                    min_qry_cover,
+                                    blast_based,
+                                    top_hits_only
+                                    )
+
+    final_og_ntd_fasta = collapse_isoforms(
+                            out_dir_of,
+                            init_og_fasta,
+                            threads = threads
+                            )
+
+    final_og_aa_fasta = oc.translate_orfs(
+                            final_og_ntd_fasta,
+                            gen_code
+                            )
+
+    back_up_file(
+        taxon_dir,
+        final_og_ntd_fasta
+        )
+
+    back_up_file(
+        taxon_dir,
+        final_og_aa_fasta
+        )
+
+
+def prep_wgs(
+        taxon_dir: str,
+        taxon_name: str,
+        taxon_code: str = None,
+        orf_db: str = None,
+        delim: str = '|',
+        og_delim: str = 'OG6',
+        gen_code: str = '1',
+        evalue: float = 1e-10,
+        min_id: int = 0,
+        max_hits: int = 1,
+        min_hit_cover: int = 60,
+        min_qry_cover: int = 0,
+        top_hits_only: bool = False,
+        blast_based: bool = True,
+        genbank: bool = True,
+        prokaryotic: bool = False,
+        remove_isoforms: bool = True,
+        threads: int = 4):
+
+    out_dir_bu = f'{taxon_dir}/Original/'
+    out_dir_oc = f'{taxon_dir}/OG_Calling/'
+
+    tax = taxon_name
+
+    if taxon_code:
+        tax = taxon_code
+
+    back_up_file(
+        out_dir_bu,
+        fasta_file
+        )
+
+    genbank = True
+
+    if genbank:
+        orf_fasta = eval_wgs_genbank(
+                        out_dir_bu,
+                        taxon_name,
+                        fasta_file,
+                        taxon_code,
+                        prokaryotic,
+                        remove_isoforms,
+                        delim
+                        )
+
+    else:
+        orf_fasta = eval_wgs_non_gbk(
+                        out_dir_bu,
+                        taxon_name,
+                        fasta_file,
+                        taxon_code,
+                        prokaryotic,
+                        delim
+                        )
+
+    peptide_fasta = oc.translate_orfs(
+                    orf_fasta,
+                    gen_code
+                    )
+
+    og_fasta, og_hit_tsv = orf_calling(
+                                out_dir_oc,
+                                tax,
+                                peptide_fasta,
+                                orf_db,
+                                delim,
+                                og_delim,
+                                gen_code,
+                                threads,
+                                evalue,
+                                min_id,
+                                max_hits,
+                                min_hit_cover,
+                                min_qry_cover,
+                                blast_based,
+                                top_hits_only,
+                                True
+                                )
+
+    og_ntd_seqs = []
+    og_pep_seqs = {i.id.rpartition(delim)[0]:i.id for i in SeqIO.parse(og_fasta,'fasta')}
+    for i in SeqIO.parse(orf_fasta, 'fasta'):
+        if i.id in og_pep_seqs:
+            i.id = og_pep_seqs[i.id]
+            i.description = ''
+            i.name = ''
+            og_ntd_seqs.append(i)
+
+    SeqIO.write(og_ntd_seqs, og_fasta.replace(".AA.fasta",".NTD.fasta"),'fasta')
+
+    back_up_file(
+        taxon_dir,
+        og_fasta
+        )
+
+    back_up_file(
+        taxon_dir,
+        og_fasta.replace(".AA.fasta",".NTD.fasta")
+        )
+
+if __name__ == '__main__':
+    try:
+        fasta_file = sys.argv[1]
+        taxon_name = sys.argv[2]
+        orf_db = sys.argv[3]
+    except:
+        print('Usage:\n    python3 phyg_add_taxa.py [FASTA-FILE] [TAXON-NAME/CODE] [ORF-DATABASE]')
+        sys.exit(1)
+
+    taxon_code = None # 'Sr_ci_Samb'
+
+    taxon_dir = f'{taxon_name}_PhyG'
+
+    if taxon_code:
+        taxon_dir = taxon_dir.replace(taxon_name, taxon_code)
+
+    transcriptome = False
+    wgs = True
+
+    if transcriptome:
+        prep_transcriptomes(
+                taxon_dir,
+                taxon_name,
+                taxon_code,
+                orf_db,
+                threads = 26
+                )
+
+    elif wgs:
+        genbank = True
+        remove_isoforms = True
+        prokaryotic = True
+        prep_wgs(
+                taxon_dir,
+                taxon_name,
+                taxon_code,
+                orf_db,
+                genbank = genbank,
+                prokaryotic = prokaryotic,
+                remove_isoforms = remove_isoforms,
+                threads = 26
+                )
--- bin/phyg_orf_related.py
+++ bin/phyg_orf_related.py
@@ -2,7 +2,10 @@
 
 import subprocess
 
+from collections import defaultdict
+
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
 
 
 def diamond_og_assign(
@@ -32,6 +35,7 @@ def diamond_og_assign(
     max_hits:       maximum number of matches to report per query sequence
     min_hit_cover:  minimum proportion of the subject covered by the query sequence
     min_qry_cover:  minimum proportion of the query sequence covered
+    prots:          FASTA file contains protein sequences
 
     Returns
     ----------
@@ -50,7 +54,7 @@ def diamond_og_assign(
                 f'-f 6 qseqid sseqid length pident qstart qend sstart send evalue bitscore qframe'
 
     if prots:
-        dmnd_cmd = dmnd_cmd.replace("blastx","blastp").rstrip(' qframe')
+        dmnd_cmd = dmnd_cmd.replace("blastx","blastp").rpartition(' qframe')[0]
 
     if min_id > 0:
         dmnd_cmd = dmnd_cmd.replace("sensitive ",f'sensitive --id {min_id} ')
@@ -60,15 +64,15 @@ def diamond_og_assign(
                             stderr=subprocess.PIPE,
                             universal_newlines=True)
 
-    return out_tsv
+    return f'{out_dir}{out_tsv}'
 
 
 def extract_orf(
         orf_coords: str,
         fasta_file: str,
-        delim: str = '|',
-        gen_code: str = '1',
+        og_delim: str = '|',
         blastx: bool = True,
+        blastp: bool = False,
         hmmer: bool = False) -> dict:
     """
     Extracts the ORF coordinates and returns FASTA files of the CDSs and corresponding
@@ -78,23 +82,28 @@ def extract_orf(
     ----------
     orf_coords:  file with ORF coordinates
     fasta_file:  FASTA formatted file
-    delim:       string delimiter for the hit -- the orthologous gene family ID needs to be after the final "split"!
+    og_delim:    string delimiter for the hit -- the orthologous gene family ID needs to be after the final "split"!
     gen_code:    genetic code for translating CDSs
     blastx:      output from DIAMOND implementation of "BLASTX"
     hmmer:       output from hmmsearch
 
     Returns
     ----------
-    orf_dict:  dictionary with extracted ORF and translation
+    orf_dict:  dictionary with extracted ORFs, their translations, and orthologous gene family
     """
 
+    # Note to self (re: delimiter): Need an "Nth" delimiter option and option for last n-characters
+    # Nth delimiter should be joined with "delim" as mandatory (make -1 by default)
+    # n-characters would supercede delim/nth-delimiter
+
     if blastx:
-        orf_dict = {line.split('\t')[0]:[(int(line.split('\t')[-1]), line.split('\t')[1].rpartition(delim)[-1], [int(line.split('\t')[4]), int(line.split('\t')[5])])] \
-                for line in open(orf_coords).readlines()}
+        orf_dict = {line.split('\t')[0]:[(og_delim + line.split('\t')[1].rpartition(og_delim)[-1], int(line.split('\t')[-1]),
+                    [int(line.split('\t')[4]), int(line.split('\t')[5])], float(line.split('\t')[-2]))] \
+                    for line in open(orf_coords).readlines()}
 
         for i in SeqIO.parse(fasta_file, 'fasta'):
             if i.id in orf_dict:
-                rf, og, coords = orf_dict[i.id][0]
+                og, rf, coords, hsp = orf_dict[i.id][0]
                 tmp_seq = i.seq[min(coords)-1:max(coords)]
                 overhang = len(tmp_seq)%3
                 if rf < 0:
@@ -102,6 +111,168 @@ def extract_orf(
                 if overhang > 0:
                     tmp_seq = tmp_seq[:-overhang]
                 orf_dict[i.id].append(tmp_seq)
-                orf_dict[i.id].append(tmp_seq.translate(gen_code))
+
+    elif blastp:
+        orf_dict = {line.split('\t')[0]:[(og_delim + line.split('\t')[1].rpartition(og_delim)[-1], float(line.split('\t')[-1]))] \
+                    for line in open(orf_coords).readlines()}
+
+        for i in SeqIO.parse(fasta_file, 'fasta'):
+            if i.id in orf_dict:
+                orf_dict[i.id].append(i.seq)
 
     return orf_dict
+
+
+def translate_orfs(
+        fasta_file: str,
+        gen_code: str) -> str:
+
+    translated_fasta = fasta_file.replace(".NTD.",".AA.")
+
+    prot_seqs = []
+    for i in SeqIO.parse(fasta_file, 'fasta'):
+        i.seq = i.seq.translate(table = int(gen_code)).rstrip("*")
+        prot_seqs.append(i)
+
+    SeqIO.write(prot_seqs, translated_fasta, 'fasta')
+
+    return translated_fasta
+
+
+def finalize_orfs(
+        out_dir: str,
+        orf_dict: dict,
+        taxon_name: str,
+        delim: str = '|',
+        top_hsp_only: bool = False,
+        prots: bool = False) -> None:
+    """
+    Store outputs from OG-assignment and ORF-calling steps
+
+    Parameters
+    ----------
+    out_dir:    directory to store FASTA files
+    orf_dict:   dictionary with extracted ORFs, their translations, and orthologous gene family
+    out_fasta:  FASTA filename to store the finalized ORFs/AAs
+    delim:      delimiter for orthologous gene family assignment in sequence name
+    prots:      orf_dict contains info for only protein sequences
+
+    Returns
+    ----------
+    None
+    """
+
+    all_seqs = []
+
+    top_hsp_seqs = []
+
+    top_hits = []
+
+    if top_hsp_only:
+        d = defaultdict(list)
+        for k, v in orf_dict.items():
+            d[v[0][0]].append((k, v[0][-1]))
+
+        for k, v in d.items():
+            top_scr = max(v, key=lambda x: float(x[-1]))
+            top_hits.append(top_scr[0])
+
+    for k, v in orf_dict.items():
+        seq_name = f'{k}{delim}{v[0][0]}'
+        if top_hsp_only:
+            if k in top_hits:
+                top_hsp_seqs.append(SeqRecord(v[-1], id = seq_name, description = '', name = ''))
+
+        all_seqs.append(SeqRecord(v[-1], id = seq_name, description = '', name = ''))
+
+    if not prots:
+        SeqIO.write(all_seqs, f'{out_dir}{taxon_name}.OG_Assigned.NTD.fasta', 'fasta')
+
+        if top_hsp_only:
+                SeqIO.write(top_hsp_seqs, f'{out_dir}{taxon_name}.OG_Assigned.Top_HSP.NTD.fasta', 'fasta')
+
+                return f'{out_dir}{taxon_name}.OG_Assigned.Top_HSP.NTD.fasta'
+
+        return f'{out_dir}{taxon_name}.OG_Assigned.NTD.fasta'
+
+    else:
+        SeqIO.write(all_seqs, f'{out_dir}{taxon_name}.OG_Assigned.AA.fasta', 'fasta')
+
+        if top_hsp_only:
+                SeqIO.write(top_hsp_seqs, f'{out_dir}{taxon_name}.OG_Assigned.Top_HSP.AA.fasta', 'fasta')
+
+                return f'{out_dir}{taxon_name}.OG_Assigned.Top_HSP.AA.fasta'
+
+        return f'{out_dir}{taxon_name}.OG_Assigned.AA.fasta'
+
+
+def orf_call_diamond(
+            out_dir: str,
+            fasta_file: str,
+            taxon_name: str,
+            diamond_db: str,
+            delim: str = '|',
+            og_delim: str = '|',
+            gen_code: str = '1',
+            threads: int = 4,
+            evalue: float = 1e-10,
+            min_id: int = 0,
+            max_hits: int = 1,
+            min_hit_cover: int = 60,
+            min_qry_cover: int = 0,
+            top_hsp_only: bool = False,
+            prots = False) -> str:
+
+    out_tsv = f'{taxon_name}.DIAMOND_Hits.tsv'
+
+    dmnd_tsv = diamond_og_assign(
+                    out_dir,
+                    out_tsv,
+                    fasta_file,
+                    diamond_db,
+                    threads,
+                    evalue,
+                    min_id,
+                    max_hits,
+                    min_hit_cover,
+                    min_qry_cover,
+                    prots
+                    )
+
+    if not prots:
+        orf_dict = extract_orf(
+                        dmnd_tsv,
+                        fasta_file,
+                        og_delim
+                        )
+
+        out_ntds = finalize_orfs(
+                    out_dir,
+                    orf_dict,
+                    taxon_name,
+                    delim,
+                    top_hsp_only,
+                    prots
+                    )
+
+        return out_ntds, dmnd_tsv
+
+    else:
+        orf_dict = extract_orf(
+                        dmnd_tsv,
+                        fasta_file,
+                        og_delim,
+                        False,
+                        True
+                        )
+
+        out_peps = finalize_orfs(
+                    out_dir,
+                    orf_dict,
+                    taxon_name,
+                    delim,
+                    top_hsp_only,
+                    True
+                    )
+
+        return out_peps, dmnd_tsv
